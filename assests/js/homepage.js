21e9d1b799a92b225fb00de043bd81a0ad8f374c
sandy <sandy-sanders@msn.com>
2021-10-12 10:11:42 -0700

    end of 6.1

--- assests/js/homepage.js
+++ assests/js/homepage.js
@@ -1,5 +1,16 @@
+var user = "facebook";
+
 var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+
 };
 
 getUserRepos();
