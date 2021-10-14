ec5f2d83cfeeabafc51abced53fb701fa5a95c71
sandy <sandy-sanders@msn.com>
2021-10-14 16:19:14 -0700

    created issues list

--- assests/js/homepage.js
+++ assests/js/homepage.js
@@ -11,6 +11,8 @@ var formSubmitHandler = function (event) {
 
     if (username) {
         getUserRepos(username);
+
+        repoContainerEl.textContent = '';
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
@@ -21,33 +23,35 @@ var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error: GitHub User Not Found");
-        }
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
 
-    })
-    .catch(function(error) {
-        //Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
+        })
+        .catch(function (error) {
+            //Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
 
-    });
+        });
 
 };
 
 var displayRepos = function (repos, searchTerm) {
     //check if api returned any repos
-    if (repos.length === 0){
+    if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
+        return;
     }
-    console.log(repos);
-    console.log(searchTerm);
-    // clear old content
-    repoContainerEl.textContent = "";
+
+
     repoSearchTerm.textContent = searchTerm;
 
     //loop over repos
@@ -74,12 +78,12 @@ var displayRepos = function (repos, searchTerm) {
         //check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML =
-                "<i class= 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         };
 
-
+        repoEl.appendChild(statusEl);
 
         //append container to the dom
         repoContainerEl.appendChild(repoEl);
